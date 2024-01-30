1bdcfe495e73b86f6f3dca2b6cefe6884cfb5963
Evan Grimes <evan.d.grimes@gmail.com>
2024-01-30 13:51:17 -0500

    reactsite initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import Navbar from '../../reactsite/src/components/Navbar';
+import Home from '../../reactsite/src/components/Pages/home';
+import { BrowserRouter as Router, Routes , Route } from 
+'react-router-dom';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <>
+       <Router>
+       <Navbar />
+       <Routes>
+       <Route path='/' exact element= {<Home/>}></Route>
+       </Routes>
+        
+       </Router>
+       </>
+        
       </header>
     </div>
   );
---
+++ src/components/HeroSection.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Button } from './Button';
+import '../App.css';
+import './HeroSection.css'
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+    <video src="../public/videos/video-2.mp4" autoPlay loop muted />
+    <h1>Get Outside!</h1>
+    <p>it's nice out. probably</p>
+    <div className='hero-btns'>
+        <Button className='btns' buttonStyle= 'btn--outline' buttonSizze= 'btn--large'>
+            Get Started 
+        </Button>
+        <Button className='btns' buttonStyle= 'btn--primary' buttonSizze= 'btn--large'>
+            Watch Trailer  <i className='far-fa-play-circle' /> 
+        </Button>
+    </div>
+
+    </div>
+  )
+}
+
+export default HeroSection
