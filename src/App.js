18f6073dc7fb57886141efd64ca7225c48b1f5cc
Evan Grimes <evan.d.grimes@gmail.com>
2024-02-01 14:13:12 -0500

    landing page fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from '../../reactsite/src/components/Navbar';
 import Home from '../../reactsite/src/components/Pages/home';
-import { BrowserRouter as Router, Routes , Route } from 
+import { BrowserRouter as Router, Routes , Route, BrowserRouter } from 
 'react-router-dom';
 import './App.css';
 
@@ -11,13 +11,13 @@ function App() {
     <div className="App">
       <header className="App-header">
        <>
-       <Router>
+       <BrowserRouter basename={process.env.PUBLIC_URL}>
        <Navbar />
        <Routes>
-       <Route path='/' exact element= {<Home/>}></Route>
+       <Route path='/'  exact element= {<Home/>}></Route>
        </Routes>
         
-       </Router>
+       </BrowserRouter>
        </>
         
       </header>
