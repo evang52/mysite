77336b86ca3eb68f6cd0ba287769c5bc23162269
Evan Grimes <evan.d.grimes@gmail.com>
2024-02-07 09:58:25 -0500

    remove redundant router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from '../../reactsite/src/components/Navbar';
 import Home from '../../reactsite/src/components/Pages/home';
 import Products from './components/Pages/products';
-import { BrowserRouter as Router, Routes , Route } from 
+import { Routes , Route } from 
 'react-router-dom';
 import './App.css';
 
@@ -12,14 +12,14 @@ function App() {
     <div className="App">
       <header className="App-header">
        <>
-       <Router basename={process.env.PUBLIC_URL}>
+       
        <Navbar />
        <Routes>
        <Route path='/'  exact element= {<Home/>}></Route>
        <Route path='/products'  exact element= {<Products/>}></Route>
        </Routes>
         
-       </Router>
+      
        </>
         
       </header>
